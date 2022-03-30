c393d1bcab1717f2937ce2ac3cb3841ab04c5f2a
David Ng <david.ng.dev@gmail.com>
2022-03-30 10:06:00 +0100

    refactor: move vendor specific solution to test file
    https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1

--- graph/dikstra-algorithm/geeksforgeeks-algorithm.test.js
+++ graph/dikstra-algorithm/geeksforgeeks-algorithm.test.js
@@ -1,6 +1,44 @@
+// https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
 import { describe, expect, it } from '@jest/globals';
 
-import { execute } from './geeksforgeeks-algorithm.js';
+function setupCostStore({ costStore, current, graph, processed }) {
+  const currentCost = costStore[current];
+
+  graph[current].forEach(([neighbor, weight]) => {
+    const existingCost = costStore[neighbor];
+    const newCost = currentCost + weight;
+    if (newCost < existingCost) costStore[neighbor] = newCost;
+  });
+  processed.push(current);
+  let nextProcessNode = null;
+  let nextProcessNodeCost = Number.POSITIVE_INFINITY;
+  costStore.forEach((cost, index) => {
+    if (cost >= nextProcessNodeCost || processed.includes(index)) return;
+    nextProcessNode = index;
+    nextProcessNodeCost = cost;
+  });
+  if (nextProcessNode !== null)
+    return setupCostStore({
+      costStore: costStore,
+      current: nextProcessNode,
+      graph: graph,
+      processed: processed,
+    });
+  return costStore;
+}
+
+function execute(numberOfNodes, graph, startPoint) {
+  const costStore = Array.from({ length: numberOfNodes }).map(
+    () => Number.POSITIVE_INFINITY,
+  );
+  costStore[startPoint] = 0;
+  return setupCostStore({
+    costStore: costStore,
+    current: startPoint,
+    graph: graph,
+    processed: [],
+  });
+}
 
 describe('Dikstra algorithm', () => {
   it('should able to find node with shortest path', () => {
